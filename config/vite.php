505f1828489bd98106141ea46143b34ce3eccdf8
mfall33 <matthewfallon33@gmail.com>
2025-09-11 19:35:45 +0100

    feat: Simplifying CSS bundling

--- config/vite.php
+++ config/vite.php
@@ -6,9 +6,7 @@ return [
     'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
     'manifestPath' => '@webroot/dist/manifest.json',
     'devServerInternal' => 'http://localhost:3000/',
-    'devServerPublic' => preg_replace('/:\d+/', '', App::env('PRIMARY_SITE_URL')) . ':3000',
+    'devServerPublic' => 'http://' . getenv('DDEV_PRIMARY_URL_HOST') . ':3000', // <-- Corrected line
     'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
     'checkDevServer' => true,
-    // 'criticalPath' => '@webroot/dist/criticalcss',
-    // 'criticalSuffix' =>'_critical.min.css',
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,38 @@
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon2';
 import ViteRestart from 'vite-plugin-restart';
 import copy from 'rollup-plugin-copy';
 import tailwindcss from "@tailwindcss/vite";
 
-export default ({ command }) => ({
-    base: command === 'serve' ? '' : '/dist/',
+export default {
+    base: '/dist/',
     publicDir: 'src/public',
     build: {
         outDir: 'web/dist/',
         emptyOutDir: true,
         sourcemap: true,
-        manifest: 'manifest.json',
         minify: 'esbuild',
         rollupOptions: {
             input: {
-                index: './src/index.js',
+                main: './src/index.js',
             },
             output: {
-                dir: 'web/dist/',
+                     dir: 'web/dist/',
+                entryFileNames: `entry.js`,  // No hash for JS entry files
+                chunkFileNames: `chunkFileNames.js`,  // No hash for JS chunks
+                assetFileNames: `[name].[ext]`
             }
         },
-    },
-    server: {
-        fs: {
-          strict: false
+        watch: {
+            include: 'src/**, templates/**'
         },
-        host: '0.0.0.0',
-        origin: 'http://localhost:3000',
-        port: 3000,
-        strictPort: true,
-        cors: true,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-		},
-		allowedHosts: true
     },
+    // Remove the server block as you don't need a dev server
     plugins: [
         tailwindcss(),
         ViteRestart({
             reload: [
                 'templates/**/*'
             ]
-        }),
-        ViteFaviconsPlugin({
-            logo: "src/public/images/favicon.png",
-            inject: false,
-            outputPath: '../favicons'
         }),
         copy({
             targets: [
@@ -58,4 +44,4 @@ export default ({ command }) => ({
         }),
         ViteImageOptimizer({})
     ]
-});
+};
